ef9c0fe5e2960a7f21901317f1340e51fbe73289
iwizz <kirill_fokin_03@list.ru>
2024-06-24 12:15:41 +1000

    pricing block

--- sait/js/main.js
+++ sait/js/main.js
@@ -1,5 +1,9 @@
+"use strict";
 const burgerButton = document.querySelector(".header__burger-button");
 const nav = document.querySelector(".header__nav");
+const switcher = document.querySelector(".pricing__head-block__switcher");
+const questions = document.querySelectorAll(".pricing__questions-block__list-item");
+
 
 burgerButton.addEventListener("click", (event) => nav.style.display = "flex")
 
@@ -7,3 +11,13 @@ nav.addEventListener("click", (event) => window.innerWidth <= 768 ? nav.style.di
 
 // window.addEventListener("resize", () => window.location.reload());
 
+
+if (switcher) {
+    switcher.addEventListener("click", () => switcher.classList.toggle("active"));
+}
+
+if(questions) {
+    for (let i of questions) {
+        i.addEventListener("click", () => i.classList.toggle("active"));
+    }
+}
